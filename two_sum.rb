fdf07b846bda470221762ec25ab2594980576340
ajrod-dev <alexander.joshua.rodriguez@gmail.com>
2024-01-24 16:49:36 -0700

    Two Sum problem added

---
+++ two_sum.rb
@@ -0,0 +1,15 @@
+def two_sum(nums, target)
+  num_indices = {}
+
+  nums.each_with_index do |num, index|
+    complement = target - num
+
+    if num_indices.key?(complement)
+      return [num_indices[complement], index]
+    end
+
+    num_indices[num] = index
+  end
+
+  return [] 
+end
