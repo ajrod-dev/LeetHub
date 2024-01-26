9272764ea67d08dd83f9f953ab9ce3120c565e17
ajrod-dev <alexander.joshua.rodriguez@gmail.com>
2024-01-26 16:25:30 -0700

    Roman Numeral to Int problem

---
+++ roman_to_int.rb
@@ -0,0 +1,28 @@
+def roman_to_int(s)
+   values = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
+  }
+
+    answer = 0
+    prior_v = 0
+
+    s.reverse.each_char do |char|
+        val = values[char]
+
+        if val < prior_v
+            answer -= val
+        else
+            answer += val
+        end
+        prior_v = val
+    end
+    answer
+end
+
+puts roman_to_int('III')
