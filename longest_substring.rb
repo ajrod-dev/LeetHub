8d6930944d3cb606ef4131fecea8e85478e7d09f
ajrod-dev <alexander.joshua.rodriguez@gmail.com>
2024-02-01 22:05:57 -0700

    longest substring problem

---
+++ longest_substring.rb
@@ -0,0 +1,26 @@
+def length_of_longest_substring(s)
+    return 0 if s.nil? || s.empty?
+
+    i = 0
+    max_length = 0
+    str = ""
+    results = {}
+
+    while i < s.length
+        char = s[i]
+        if str.include?(char)
+            max_length = [max_length, str.length].max
+            str = str[str.index(char) + 1..-1]
+        end
+        str += char
+        i += 1
+    end
+
+    max_length = [max_length, str.length].max  # Update max_length after the loop completes
+end
+
+# Example usage:
+puts length_of_longest_substring("dvdf")  # Output: 3
+
+
+puts length_of_longest_substring("au")
